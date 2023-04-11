d6d4a761a416a96a55ab2628927034af117d7e35
nhn010195 <71431032+nhn010195@users.noreply.github.com>
2023-04-12 00:45:15 +0700

    12/4/2023

--- view/Shared/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require '../../xuly/Shared/connect_db_view_after.php';
-    session_unset();
-    session_destroy();
-?>
-<script>
-    alert('Đăng xuất thành công');
-    document.location = "../../index.php";
-</script>
