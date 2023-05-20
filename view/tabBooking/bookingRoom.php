edcd767b038b633089fe72ae721fe59bbc9822b0
LeMinhMan <minhman.le11@gmail.com>
2023-05-20 20:32:35 +0700

    Xem danh sách đặt phòng, Xem danh sách đặt dịch vụ
    Xem danh sách đặt phòng, Xem danh sách đặt dịch vụ

--- view/tabBooking/bookingRoom.php
+++ view/tabBooking/bookingRoom.php
@@ -1,6 +1,7 @@
 <?php require '../Shared/head_after.php'; ?>
 <?php require '../../xuly/Shared/connect_db_view_after.php'; ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="../../style/js/search.js"></script>
 <body>
     <!-- Page Wrapper -->
     <div id="wrapper">
@@ -62,9 +63,18 @@
                             <div class="d-flex justify-content-between">
                                 <h6 style="font-weight: bolder; font-size:25px;" class="m-0 text-primary">Danh sách đặt
                                     phòng</h6>
-                                <form action="/bookingRoom/searchName">
+                                <a href="../room/room.php">
+                                    <button class="btn btn-info btn-fill pull-right">Đặt
+                                            phòng</button>                                   
+                                </a>
+                            </div>
+                        </div>
+                        <div class="card-header py-3">
+                            <div class="d-flex justify-content-end">
+                                <form>
                                     <div class="input-group">
-                                        <input style="width:700px" type="text" name="q" class="form-control mr-6 w-10" placeholder="Search">
+                                        <input style="width:500px" type="text" name="q" class="form-control mr-6 w-10"
+                                            placeholder="Search" onkeyup="Search(this.value,'DsDat')">
                                     </div>
                                 </form>
                             </div>
@@ -72,31 +82,76 @@
                         <div class="card-body">
                             <div id="tablezone" class="table-responsive">
 
-
-
                                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                     <thead id="headerzone">
                                         <tr>
                                             <th>Số phòng</th>
+                                            <th>Ngày đặt phòng</th>
                                             <th>Loại phòng</th>
                                             <th>Tên khách hàng</th>
                                             <th>Số điện thoại</th>
                                             <th>CMND</th>
-                                            <th>Ngày nhận phòng</th>
-                                            <th>Ngày Trả phòng</th>
-                                            <th>Xác nhận</th>
+                                            <th>Trạng thái</th>
+                                            <th>Thao tác</th>
                                             <th>Huỷ</th>
                                         </tr>
                                     </thead>
 
-                                    <tbody id="datazone">
+                                    <tbody id="search-table">
                                         <?php require '../../xuly/xl_xem_datphong.php'; ?>
                                     </tbody>
                                 </table>
-                                <a href="../room/room.php">
-                                    <button class="btn btn-info btn-fill pull-right">Đặt
-                                            phòng</button>                                   
-                                </a>
+                                <nav id="page_num">
+                                    <ul class="pagination justify-content-end">
+                                        <?php 
+                                            if($page == 1)
+                                            {
+                                                ?>
+                                                <li class="page-item disabled">
+                                                    <a class="page-link">Previous</a>
+                                                </li>
+                                                <?php
+                                            }
+                                            else
+                                            {                                            
+                                                ?>
+                                                <li class="page-item">
+                                                    <a class="page-link" href="./bookingRoom.php?page=<?php echo ($page - 1); ?>">Previous</a>
+                                                </li>
+                                                <?php
+                                            }
+                                        ?>
+
+                                        <?php
+                                            for($page_num = 1; $page_num <= $number_of_page; $page_num++)
+                                            {
+                                                ?>
+                                                <li class="page-item <?php if($_GET['page'] == $page_num) echo "active"; else if(!isset ($_GET['page']) && $page_num == 1) echo "active"; ?>"><a class="page-link" href="./bookingRoom.php?page=<?php echo $page_num; ?>"><?php echo $page_num; ?></a></li>
+                                                <?php
+                                            }
+                                        ?>
+
+                                        <?php
+                                            
+                                            if($page == $number_of_page) 
+                                            {
+                                                ?>
+                                                <li class="page-item disabled">
+                                                    <a class="page-link" href="#">Next</a>
+                                                </li>
+                                                <?php
+                                            }
+                                            else
+                                            {
+                                                ?>
+                                                <li class="page-item">
+                                                    <a class="page-link" href="./bookingRoom.php?page=<?php echo ($page + 1); ?>">Next</a>
+                                                </li>
+                                                <?php
+                                            }
+                                        ?>                                 
+                                    </ul>
+                                </nav>
                             </div>
                         </div>
                     </div>
@@ -106,56 +161,7 @@
             <!-- End of Main Content -->
         </div>
 
-        <!--confirm-->
-        <div id="delete-cusomter-modal" class="modal" tabindex="-1">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Xoá khách hàng</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Bạn có chắc chắn muốn xoá khách hàng này !!! </p>
-                    </div>
-                    <div class="modal-footer">
-                        <button id="btn-delete-customer" type="button" class="btn btn-danger">Xoá bỏ</button>
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Huỷ</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!--form delete course -->
-        <form method="POST" name="delete-customer-form"></form>
-
-        <!--Script callback function to delete-->
-        <script>
-            document.addEventListener("DOMContentLoaded", function() {
-
-                var customerId;
-                var deleteForm = document.forms['delete-customer-form'];
-                var btnDeleteCustomer = document.getElementById('btn-delete-customer');
-
-                //when dialog confirm click
-                $('#delete-cusomter-modal').on('show.bs.modal', function(event) {
-
-                    var button = $(event.relatedTarget) // Button that triggered the modal
-                    customerId = button.data('id') // Extract info from data-* attributes
-                });
-
-                btnDeleteCustomer.onclick = function() {
-                    deleteForm.action = `/bookingRoom/${customerId}?_method=DELETE`;
-                    deleteForm.submit();
-                }
-            });
-        </script>
         <script type="text/javascript">
-            function Confirm(dp_id, room_id) {
-                $.post("../../xuly/xl_confirm_datphong.php", {MaDP: dp_id, MaPhong: room_id}, function(returndata){
-                    if(returndata == 1)                   
-                        $("#dataTable").load(window.location.href + " #dataTable > *");
-                })
-            }
             function Cancel(dp_id, room_id) {
                 $.post("../../xuly/xl_huyphong.php", {MaDP: dp_id, MaPhong: room_id}, function(returndata){
                     if(returndata == 1)                  
